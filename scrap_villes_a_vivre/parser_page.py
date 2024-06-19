2560773dcbc73c44ea4de8356c527dbd33c5ba47
JeannotJeannot <me@noboby.com>
2024-06-19 22:07:31 +0200

    reducing...

--- scrap_villes_a_vivre/parser_page.py
+++ scrap_villes_a_vivre/parser_page.py
@@ -1,48 +1,15 @@
 """Define the parser for the webpage."""
 
-import logging
-from typing import Self
-
-import bs4
-from bs4 import BeautifulSoup
+from typing import TYPE_CHECKING
 
 from scrap_villes_a_vivre.page_request import get_page
-from scrap_villes_a_vivre.parsers_sections import GetSectionParser, SectionParser, FullPageParser
+from scrap_villes_a_vivre.parsers_sections import FullPageParser
 
-
-class PageParser:
-    """Base class for all page parsers."""
-
-    page: BeautifulSoup
-
-    def __init__(self: Self, page: BeautifulSoup) -> None:
-        """Initiate PageParser.
-
-        It needs page as an beautifulsoup to extract informations from.
-        """
-        self.page = page
-
-    def get_all_sections(self: Self) -> bs4.element.ResultSet:
-        """Return all sections usefull in the web page."""
-        if result := self.page.find_all(name="section"):
-            return result
-        message: str = "No section found !"
-        raise ValueError(message)
-
-    def get_informations(self: Self) -> list[str]:
-        """Return informations about one page."""
-        sections: bs4.element.ResultSet = self.get_all_sections()
-        informations: list[str] = FullPageParser(self.page).parse()
-        # for section in sections:
-        #     message: str = f"Handling {section['id']}"
-        #     logging.warning(message)
-        #     parser: type[SectionParser] = GetSectionParser.get_parser(section["id"])
-        #     informations.extend(parser(section).parse())
-
-        return informations
+if TYPE_CHECKING:
+    from bs4 import BeautifulSoup
 
 
 def get_informations_from_url(page_url: str) -> list[str]:
     """Return informations about one url."""
     page_souped: BeautifulSoup = get_page(page_url)
-    return PageParser(page_souped).get_informations()
+    return FullPageParser(page_souped).parse()
--- scrap_villes_a_vivre/parsers_sections.py
+++ scrap_villes_a_vivre/parsers_sections.py
@@ -1,41 +1,30 @@
 """Define parsers for each sections of the website."""
 
-import logging
-from abc import ABC, abstractmethod
 from typing import ClassVar, Self
 
 from bs4.element import ResultSet, Tag
 
 
-class SectionParser(ABC):
-    """Base class for all section parsers."""
+class FullPageParser:
+    """Parser for balises only."""
 
-    _section: Tag
+    _page: Tag
+    _info_balise: ClassVar[set[str]] = {
+        "demo-content",
+        "bar",
+        "dynamic-content",
+        "text-content",
+        "second-circle-content",
+        "card-content",
+        "city-crime",
+    }
 
-    def __init__(self: Self, section: Tag) -> None:
+    def __init__(self: Self, page: Tag) -> None:
         """Initiate SectionParser.
 
         It needs section as an beautifulsoup.tag to extract informations from.
         """
-        self._section = section
-
-    @abstractmethod
-    def parse(self: Self) -> list[str]:
-        """Parse section and return the list of formatted information."""
-
-
-class SectionParserDummy(SectionParser):
-    """Parser for useless section. It parses nothing."""
-
-    def parse(self: Self) -> list[str]:
-        """Parse section and return the list of formatted information."""
-        return []
-
-
-class SectionParserBalises(SectionParser):
-    """Parser for balises only."""
-
-    _info_balise: ClassVar[set[str]]
+        self._page = page
 
     def _filter(self: Self, tag: Tag) -> bool:
         """Filter the information for each tag in the economie section."""
@@ -49,7 +38,7 @@ class SectionParserBalises(SectionParser):
 
         Returned informations are not formated.
         """
-        if result := self._section.find_all(self._filter):
+        if result := self._page.find_all(self._filter):
             return result
         message = "No information found !"
         raise ValueError(message)
@@ -60,85 +49,3 @@ class SectionParserBalises(SectionParser):
             " ".join((info.text.strip()).split())
             for info in self._get_all_informations()
         ]
-
-
-class SectionParserPopulation(SectionParserBalises):
-    """Parser for population section."""
-
-    _info_balise: ClassVar[set[str]] = {"demo-content", "bar"}
-
-
-class SectionParserPresentation(SectionParserBalises):
-    """Parser for Presentation section."""
-
-    _info_balise: ClassVar[set[str]] = {"dynamic-content"}
-
-
-class SectionParserEconomie(SectionParserBalises):
-    """Parser for Economie section."""
-
-    _info_balise: ClassVar[set[str]] = {"text-content"}
-
-
-class SectionParserImmobilier(SectionParserBalises):
-    """Parser for Economie section."""
-
-    _info_balise: ClassVar[set[str]] = {"second-circle-content", "card-content", "bar"}
-
-
-class SectionParserSecurite(SectionParserBalises):
-    """Parser for Economie section."""
-
-    _info_balise: ClassVar[set[str]] = {"city-crime"}
-
-
-class SectionParserServices(SectionParserBalises):
-    """Parser for Economie section."""
-
-    _info_balise: ClassVar[set[str]] = {""}
-
-
-class SectionParserNearBy(SectionParserBalises):
-    """Parser for Economie section."""
-
-    _info_balise: ClassVar[set[str]] = {""}
-
-class FullPageParser(SectionParserBalises):
-    _info_balise: ClassVar[set[str]] = {"demo-content", "bar", "dynamic-content","text-content", "second-circle-content", "card-content", "bar", "city-crime"}
-
-
-class GetSectionParser:
-    """Handle the choosing of the section parser."""
-
-    _parsers: ClassVar[dict[str, type[SectionParser]]] = {
-        "economie": SectionParserEconomie,
-        "population": SectionParserPopulation,
-        "presentation": SectionParserPresentation,
-        "immobilier": SectionParserImmobilier,
-        "securite": SectionParserSecurite,
-        "services": SectionParserServices,
-        "near-by": SectionParserNearBy,
-        "climat": SectionParserDummy,
-        "labels": SectionParserDummy,
-        "politique": SectionParserDummy,
-        "cityhall": SectionParserDummy,
-        "compare": SectionParserDummy,
-        "reviews": SectionParserDummy,
-    }
-
-    @classmethod
-    def get_handled_parsers(cls: type["GetSectionParser"]) -> list[str]:
-        """Return the list of handle sections."""
-        return list(cls._parsers.keys())
-
-    @classmethod
-    def get_parser(
-        cls: type["GetSectionParser"],
-        section_name: str,
-    ) -> type[SectionParser]:
-        """Return needed parser for selected section."""
-        if section_name not in cls._parsers:
-            message: str = f"Parser for {section_name} is not defined."
-            logging.warning(message)
-            return SectionParserDummy
-        return cls._parsers[section_name]
